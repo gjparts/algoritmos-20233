a8b6c862b1ea7e28e83e94541a91a5b8340fd325
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-10-30 09:59:40 -0600

    commit 3010202315656

---
+++ prueba arreglo.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arreglo1[10];
+	
+	for( int i = 0; i < end(arreglo1)-begin(arreglo1); i++ )
+		cout << arreglo1[i] << endl;
+	
+	return 123;
+}
