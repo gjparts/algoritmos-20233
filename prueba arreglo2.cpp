407593d479f935ea7e78d3ab23e877b89158a93a
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-11-01 11:03:28 -0600

    commit 011120232621

---
+++ prueba arreglo2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int procesar(int arreglo[]){
+	return arreglo[0];
+}
+
+int main(){
+	int a[] = {8,5,7};
+	cout << procesar(a) << endl;
+	
+	return 456;
+}
+
