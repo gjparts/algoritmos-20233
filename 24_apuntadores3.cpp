f2f75a122912818b332b8880d4cee9df7eebc174
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-11-09 09:06:49 -0600

    commit 091120238719

--- 24_apuntadores3.cpp
+++ 24_apuntadores3.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 #include<cmath>
+#include<time.h>
 using namespace std;
 
 //prototipo de funcion
 void arregloMitad( float *arreglo, int tamano);
+void arregloAlCuadrado( double *arreglo, int tamano );
+void llenarArreglo( int *arreglo, int tamano );
 
 int main(){
 	//Uso de apuntadores en arreglos
@@ -24,6 +27,22 @@ int main(){
 		cout << arr[i] << endl;
 	}
 	
+	double arreglox[] = {2.5, 3, 4.8, 9.4};
+	cout << "antes de evelar al cuadrado: " << endl;
+	for( int i = 0; i < end(arreglox)-begin(arreglox); i++ ){
+		cout << arreglox[i] << endl;
+	}
+	arregloAlCuadrado(arreglox, end(arreglox)-begin(arreglox) );
+	cout << "despues de evelar al cuadrado: " << endl;
+	for( int i = 0; i < end(arreglox)-begin(arreglox); i++ ){
+		cout << arreglox[i] << endl;
+	}
+	int arregloz[15];
+	llenarArreglo(arregloz, end(arregloz)-begin(arregloz) );
+	for( int i = 0; i < end(arregloz)-begin(arregloz); i++ ){
+		cout << arregloz[i] << endl;
+	}
+	
 	return 765;
 }
 
@@ -34,3 +53,27 @@ void arregloMitad( float *arreglo, int tamano){
 		arreglo[i] = arreglo[i]/2;
 	}
 }
+/*
+Hacer una funcion void que le enviemos un arreglo apuntado
+	de numeros double y que le altere todos sus elementos
+	elevando cada uno de ellos al cuadrado.
+	desde el main: imprimir el arreglo antes y despues
+	de aplicarle la funcion*/
+void arregloAlCuadrado( double *arreglo, int tamano ){
+	for( int i = 0; i < tamano; i++ ){
+		arreglo[i] = arreglo[i]*arreglo[i]; //arreglo[i] = pow(arreglo[i],2);
+	}
+}
+
+/*hacer una funcion void que llene todos los elementos de
+un arreglo apuntado de numeros enteros con numeros
+al azar entre 1 y 99.*/
+void llenarArreglo( int *arreglo, int tamano ){
+	//generador de numeros al azar
+	srand( time(NULL) );
+	for( int i = 0; i < tamano; i++ ){
+		arreglo[i] = rand()%(99-1+1)+1;
+	}
+}
+
+
---
+++ 25_apuntadores4.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+//prototipo de funcion
+void mayusculas(string *str);
+
+int main(){
+	/*apuntadores cuando se usa objetos o estructuras de datos.
+	a) al declarar la funcion se usa el signo de apuntador * para el objeto/estructura.
+	b) al usar la variable en el ambito externo se usa el simbolo del apuntador
+	   para el objeto/estructura.
+	c) en el ambito externo no se tiene acceso a los atributos y metodos del objeto/estructura
+	   para solucionarle se recomienda sacar una copia del objeto/estructura en el
+	   ambito externo, trabajar sobre la copia y por ultimo sobreescribir al apuntador
+	   con la copia.
+	d) al usar la funcion enviar el objeto/estructura con el & */
+	   
+	string str1 = "Gerardo Portillo";
+	cout << "str1: " << str1 << endl;
+	cout << "direccion de memoria de str1: " << &str1 << endl;
+	mayusculas(&str1);
+	cout << "str1 luego de mayusculas: " << str1 << endl;
+	
+	return 123;
+}
+
+/*hacer una funcion void que reciba un string apuntado y lo cambie a mayusculas*/
+void mayusculas(string *str){
+	//sacar una copia local del valor del apuntador
+	string copia = *str;
+	//pasar la copia local a mayusculas
+	transform(copia.begin(), copia.end(), copia.begin(), ::toupper);
+	//una vez alterada la copia, sobreescribir el valor de la variable apuntada
+	//por el valor de la copia.
+	*str = copia;
+}
+
+
+
+
+
+
